a15fec07750fa784e18d1e5a6025fc2e93c23683
Falik <profissionalfalik@gmail.com>
2022-05-05 13:22:48 -0300

    Upando

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Retangulo retangulo = new Retangulo();
+        Scanner sc = new Scanner(System.in);
+        retangulo.Height = sc.nextDouble();
+        retangulo.Width = sc.nextDouble();
+        System.out.println(retangulo.Area());
+        System.out.println(retangulo.Perimeter());
+        System.out.println(retangulo.Diagonal());
+        sc.close();
+    }
+}
---
+++ Retangulo.java
@@ -0,0 +1,15 @@
+public class Retangulo {
+
+    public double Width;
+    public double Height;
+
+    public double Area() {
+        return Width * Height;
+    }
+    public double Perimeter() {
+        return 2*Width + 2*Height;
+    }
+    public double Diagonal() {
+        return Math.sqrt(Math.pow(Width, 2) + Math.pow(Height, 2));
+    }
+}
